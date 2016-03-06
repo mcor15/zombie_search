888a226d4fb9156fd120282066a589121ed357da
Hailey Cooper <Hailey Cooper>
2016-03-06 20:43:04 +0000

    bootstrap

--- zombie_search_project/zombie_search/views.py
+++ zombie_search_project/zombie_search/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    totalKills_list = User.objects.order_by('totalKills')[:10]
-    gameKills_list = User.objects.order_by('maxKills')[:10]
-    totalDays_list = User.objects.order_by('totalDays')[:10]	
-    averageDays_list = User.objects.order_by('averageDays')[:10]
+#    totalKills_list = user.objects.order_by('totalKills')[:10]
+#    gameKills_list = user.objects.order_by('maxKills')[:10]
+#    totalDays_list = user.objects.order_by('totalDays')[:10]	
+#    averageDays_list = user.objects.order_by('averageDays')[:10]
 	
-    context_dict = {'totalKills': totalKills_list, 
-					'gameKills': gameKills_list,
-					'totalDays': totalDays_list,
-					'averageDays': averageDays_list,}
-	
-    return render(request, 'zombie_search/Home.html', context_dict)
+#    context_dict = {'totalKills': totalKills_list, 
+#					'gameKills': gameKills_list,
+#					'totalDays': totalDays_list,
+#					'averageDays': averageDays_list,}
+#	
+#   return render(request, 'zombie_search/Home.html', context_dict)
+	return render(request,'zombie_search/Base.html')
 	
 def about(request):
 	return HttpResponse("Instructions")
