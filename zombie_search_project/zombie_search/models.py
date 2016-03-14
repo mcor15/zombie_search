6b79edc66b9d150b3f3954a6521b9bf0d15f49a2
Matthew <mcornetto1@yahoo.com>
2016-03-14 17:33:50 +0000

    fix slug in Player model and change avg days to FloatField in Player model

--- zombie_search_project/zombie_search/models.py
+++ zombie_search_project/zombie_search/models.py
@@ -7,32 +7,34 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     #One to one mapping of Player model to default User model
     user = models.OneToOneField(User)
-		
-    profile_picture = models.ImageField(upload_to='player_avatars', blank=True)
-	
+
+    profile_picture = models.ImageField(upload_to='/static/player_avatars', blank=True)
+
     games_played = models.IntegerField(default=0)
     total_days = models.IntegerField(default=0)
-    avg_days = models.IntegerField(default=0)
+    avg_days = models.FloatField(default=0.0)
     most_days_survived = models.IntegerField(default=0)
     most_kills = models.IntegerField(default=0)
     total_kills = models.IntegerField(default=0)
     most_people = models.IntegerField(default=0)
-	
     slug = models.SlugField(unique=True)
-		
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.user.username)
+        super(Player, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return self.user.username
-	
+
 class Badge(models.Model):
     type = models.CharField(max_length=10)
     description = models.CharField(max_length=20)
     criteria = models.IntegerField(default=0)
     level = models.IntegerField(default=0)
-    icon = models.ImageField()
+    icon = models.ImageField()#fix me, PLEASE!
 
     def __unicode__(self):
         return self.type
-		
+
 class Achievement(models.Model):
     player = models.ForeignKey(Player)
     badge = models.ForeignKey(Badge)
